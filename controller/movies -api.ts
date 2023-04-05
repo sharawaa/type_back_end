a85c318331a28ce609c53087c76abba8f8c7d395
Morning <morning@admins-iMac-1-7.local>
2023-04-05 13:31:44 +0800

    05

---
+++ controller/movies -api.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from "express";
+import { request } from "http";
+import movieModel from "../model/theater-model";
+
+const moviesRouter = express.Router();
+
+moviesRouter.get("/movies", async (req: Request, res: Response) => {
+  const result = await movieModel
+    .find({ poster: { $exists: 1 } })
+    .select({ title: 1, poster: 1, _id: 1 })
+    .limit(100);
+  res.status(200).send(result);
+});
+
+moviesRouter.get(`/movie/:id`, async (request: Request, response: Response) => {
+  console.log(request);
+  const result = await movieModel.findOne({ _id: `${request.params.id}` });
+  response.status(200).send(result);
+});
+
+export default moviesRouter;
--- index.ts
+++ index.ts
@@ -2,14 +2,14 @@ import express, { Express, Request, Response } from "express";
 import "./config/mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-import theaterRouter from "./controller/theater -api";
+import moviesRouter from "./controller/movies -api";
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
 app.use(cors());
-app.use(theaterRouter);
+app.use(moviesRouter);
 
 app.listen(port, () => {
   console.log(`⚡️ [server]: Server is running at http://localhost:${port}`);
